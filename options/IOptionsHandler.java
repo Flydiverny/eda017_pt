f29e1a6eda9f32d96ad3bf79e8ceec2d50797402
Markus Maga <markus@nadilus.com>
2013-02-10 21:21:02 +0100

    Ops

--- options/IOptionsHandler.java
+++
@@ -1,9 +0,0 @@
-package se.markusmaga.lth.pt.helper.options;
-
-public interface IOptionsHandler {
-	//public boolean add(Option o);
-	//public void changeOptions();
-	//public Option getOption(String flag);
-	//public boolean hasOption(String flag);
-	//public boolean hasOptions();
-}
