76f0656d6a4fa28d7c88eadc171749a4635640a8
Markus Maga <markus@nadilus.com>
2013-02-19 21:12:00 +0100

    Added goal turtle to turtle race

--- inlmn6/src/se/markusmaga/lth/pt/race/RaceWindow.java
+++ inlmn6/src/se/markusmaga/lth/pt/race/RaceWindow.java
@@ -3,13 +3,17 @@ package se.markusmaga.lth.pt.race;
 import se.markusmaga.lth.pt.race.turtles.*;
 
 import se.lth.cs.pt.graphics.basic.GraphicsWindow;
+import se.lth.cs.pt.io.Keyboard;
 
 public class RaceWindow {
-	private static final double	LINE_TOP_Y		= 50;
-	private static final double	LINE_BOT_Y		= 250;
-	private static final double	START_LINE_X	= 50;
-	private static final double	FINISH_LINE_X	= 450;
+	private final double LINE_OFFSET_Y;
+	private final double LINE_BOT_Y;
+	private final double LINE_OFFSET_X;
+	private final double LINE_GOAL_X;
 
+	private int windowWidth;
+	private int windowHeight;
+	
 	private TurtleRace turtleRace;
 	private int nbrOfTurtles;
 	private GraphicsWindow gw;
@@ -17,23 +21,44 @@ public class RaceWindow {
 	private List<RaceTurtle> racers;
 	
 	public RaceWindow(int width, int height) {
+		this(width, height, 50, 50);
+	}
 	
-		this.gw = new GraphicsWindow(width, height);
-		this.nbrOfTurtles = getAmountOfTurtles();
+	public RaceWindow(int width, int height, int offsetY, int offsetX) {
+		this.windowWidth = width;
+		this.windowHeight = height;
+	
+		this.LINE_OFFSET_Y = offsetY;
+		this.LINE_BOT_Y = this.windowHeight-LINE_OFFSET_Y;
+		
+		this.LINE_OFFSET_X = offsetX;
+		this.LINE_GOAL_X = this.windowWidth-LINE_OFFSET_X;
 		
 		this.racers = new LinkedList<RaceTurtle>();
+	
+		// Start stuff
+		this.nbrOfTurtles = getAmountOfTurtles();
+		
+		this.gw = createWindow(windowWidth, windowHeight);
 	}
 	
 	/**
-	 * Skapar ett fönster
+	 * Creats a graphic window and draws goal line as well as start line.
 	 * @param width Fönster bredd
 	 * @param height Fönster höjd
 	 */
 	private GraphicsWindow createWindow(int width, int height) {
 		GraphicsWindow w = new GraphicsWindow(width, height);
 		
-		w.drawLine(START_LINE_X, LINE_TOP_Y, START_LINE_X, LINE_BOT_Y, 2, Color.RED);
-		w.drawLine(FINISH_LINE_X, LINE_TOP_Y, FINISH_LINE_X, LINE_BOT_Y, 2, Color.RED);	
+		w.drawLine(LINE_OFFSET_X, LINE_OFFSET_Y, LINE_OFFSET_X, LINE_BOT_Y, 2, Color.RED);
+
+		int offset = 25;
+		
+		GoalTurtle g = new GoalTurtle(w, LINE_GOAL_X, -offset);
+		g.left(180); // look down
+		g.forward(LINE_OFFSET_Y+offset); // walk in
+		g.drawGoal(LINE_BOT_Y-LINE_OFFSET_Y); // draw
+		g.forward(LINE_OFFSET_Y+offset); // walk out
 
 		return w;
 	}
---
+++ inlmn6/src/se/markusmaga/lth/pt/race/TurtleRace.java
@@ -0,0 +1,8 @@
+package se.markusmaga.lth.pt.race;
+
+import se.markusmaga.lth.pt.race.turtles.*;
+
+import se.lth.cs.pt.graphics.basic.GraphicsWindow;
+
+public class TurtleRace {
+}
